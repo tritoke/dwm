3ae866be7032630b04377e51b0dbaf4de48f400c
Sam <tritoke@protonmail.com>
2021-07-23 20:33:01 +0100

    Added rotatetags bindings to config.h

--- config.h
+++ config.h
@@ -136,6 +136,8 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_period,                focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,                 tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period,                tagmon,         {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_Left,                  rotatetags,     {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_Right,                 rotatetags,     {.i = +1 } },
 	TAGKEYS(                       	XK_1,                                     0)
 	TAGKEYS(                       	XK_2,                                     1)
 	TAGKEYS(                       	XK_3,                                     2)
