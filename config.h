1bd95dcc591be983efe955ec929406f8bc0a46d1
Sam <tritoke@protonmail.com>
2023-04-28 13:01:04 +0100

    Change which monitors apps display on.

--- config.h
+++ config.h
@@ -53,8 +53,8 @@ static const Rule rules[] = {
 	/* class                     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
 	{ "st",                      NULL,     NULL,           0,         0,          1,          -1,        -1 },
 	{ "firefoxdeveloperedition", NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
-	{ "discord",                 NULL,     NULL,           1 << 7,    0,          0,           0,        -1 },
-	{ "Spotify",                 NULL,     NULL,           1 << 6,    0,          0,           0,        -1 },
+	{ "discord",                 NULL,     NULL,           1 << 7,    0,          0,           0,         2 }, /* open on my right monitor */
+	{ "Spotify",                 NULL,     NULL,           0,         0,          0,           0,         1 }, /* open on my left monitor on my desktop */
 	{ NULL,                      NULL,     "Event Tester", 0,         1,          0,           1,        -1 }, /* xev */
 };
 
