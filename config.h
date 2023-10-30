9e42819c44b59ad24de2ac9f68b286f94b482fb1
Sam Leonard <sam.leonard@codethink.co.uk>
2023-10-30 17:24:18 +0000

    🔥 removed termbigcmd

--- config.h
+++ config.h
@@ -96,7 +96,6 @@ static const Layout layouts[] = {
 static const char dmenu_highpriority[]    = "spotify,discord,firefox-developer-edition,wireshark,ghidra,google-chrome-stable,zoom";
 static const char *dmenucmd[]             = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_base00, "-nf", col_base0D, "-sb", col_base08, "-sf", col_base0D, "-hb", col_base0D, "-hf", col_base00, "-hp", dmenu_highpriority, NULL };
 static const char *termcmd[]              = { "st", NULL };
-static const char *termbigcmd[]           = { "st", "-f", "JetBrains Mono:size=12", NULL };
 static const char *brightness_up[]        = { "xbacklight", "-inc", "5", NULL };
 static const char *brightness_down[]      = { "xbacklight", "-dec", "5", NULL };
 static const char *volume_up[]            = { VOLUME_UP(1), NULL };
@@ -125,7 +124,6 @@ static Key keys[] = {
 	/* modifier                     key                       function        argument */
 	{ MODKEY,                       XK_p,                     spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return,                spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_Return,                spawn,          {.v = termbigcmd } },
 	{ MODKEY,                       XK_Print,                 spawn,          screenshot },
 	{ MODKEY|ShiftMask,             XK_s,                     spawn,          screenshot },
 	{ MODKEY|ShiftMask,             XK_Print,                 spawn,          {.v = screenshot_window } },
